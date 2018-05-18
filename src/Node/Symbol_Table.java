ad595e6f7a578a85244d1671bfd922c422a2a704
dcaceres7 <dancacers_95@unitec.edu>
2018-05-17 19:49:37 -0600

    correccion en declaracion de funciones

---
+++ src/Node/Symbol_Table.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Node;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Symbol_Table {
+    protected String id;
+    protected String type;
+    
+    Symbol_Table(){
+        id = "";
+        type ="";
+    }
+    
+   Symbol_Table(String id, String type){
+       this.id = id;
+       this.type = type;
+   }
+}
